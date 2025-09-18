8f1d608135759726699d73f50f0682a83e047456
shriram-cmyk <shriram@vidocto.com>
2025-09-18 12:57:52 +0530

    Main.ts file cors access changed

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://your-frontend-domain.com'],
+    origin: ['http://localhost:3000', 'https://your-frontend-domain.com'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
